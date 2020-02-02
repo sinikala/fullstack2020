f42d569aca94897c3c4f6404487eb34422c30a54
Laura S <46318664+sinikala@users.noreply.github.com>
2020-02-02 14:10:09 +0200

    puhelinluettelo full stack

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = '/api/persons'
+//'https://infinite-beach-82485.herokuapp.com/api/persons'
+//'http://localhost:3001/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
